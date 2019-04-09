e3e12769ece43d9d956207a7433a94f6ac3d2230
MrRen <543169072@qq.com>
2019-04-09 19:04:32 +0800

    init

--- src/api.php
+++ src/api.php
@@ -8,41 +8,10 @@
 
 namespace epii\app;
 
-use epii\server\Response;
 
-abstract class api
+
+abstract class api extends \epii\server\api
 {
-    protected function getNoNeedAuth(): array
-    {
-        return [];
-    }
 
-    abstract protected function doAuth(): bool;
-
-    public function init()
-    {
-        $no = $this->getNoNeedAuth();
-        if (count($no) > 0) {
-            $m = \epii\server\App::getInstance()->getRunner()[1];
-            if (!in_array($m, $no)) {
-                if (!$this->doAuth()) {
-                    $this->error("授权失败");
-                }
-
-            }
-        }
-
-    }
-
-    protected function success($data = null, $msg = '', $code = 1, $type = null, array $header = [])
-    {
-        Response::success($data, $msg, $code, $type, $header);
-    }
-
-
-    protected function error($msg = '', $data = null, $code = 0, $type = null, array $header = [])
-    {
-        Response::error($msg, $data, $code, $type, $header);
-    }
 
 }
